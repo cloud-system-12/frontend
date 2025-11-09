abae940ab731e4c01922d4054dd59b45648b9a8c
jungwoow <yeriel.390@gmail.com>
2025-11-09 16:50:46 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen text-black">
+      <h1 className="text-5xl font-bold mb-6">Tailwind 설치 완료!</h1>
+    </div>
+  );
+}
+
+export default App;
